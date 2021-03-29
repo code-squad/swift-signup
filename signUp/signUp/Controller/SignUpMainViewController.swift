921d6db5015ea88e4f9b87fc4d4f40e4efe94331
Ray suh <readingod@naver.com>
2021-03-29 14:42:58 +0900

    Main View 배경화면 color변경

---
+++ signUp/signUp/Controller/SignUpMainViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class SignUpMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpMainView()
+    }
+    
+    private func setUpMainView() {
+        view.backgroundColor = UIColor.systemGray5
+    }
+}
+

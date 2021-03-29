0d95de316e17765cf9493cbe2a2b22113e0a59b1
HongzCloud <jihong21511@gmail.com>
2021-03-30 00:35:54 +0900

    💄 Add TextFieldDelegate
    텍스트필드를 동적으로 바꾸기 위해 델리게이트를 만들어 줬다.

---
+++ SignUp/SignUp/TextFieldDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  TextFieldDelegate.swift
+//  SignUp
+//
+//  Created by 오킹 on 2021/03/29.
+//
+
+import UIKit
+
+class TextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.systemBlue.cgColor
+    }
+  
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderColor = CGColor(red: 112, green: 112, blue: 112, alpha: 1)
+    }
+}
+

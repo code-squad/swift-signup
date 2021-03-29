50b03dfb6527b73006cbd99ff3d812c80fee4334
Ray suh <readingod@naver.com>
2021-03-29 18:09:22 +0900

    다음 버튼 누르면 뷰컨트롤러 이동 구현

---
+++ signUp/signUp/Controller/PrivacyViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class PrivacyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemRed
+    }
+    
+}
--- signUp/signUp/View/MoveToNextPage.swift
+++ signUp/signUp/View/MoveToNextPage.swift
@@ -1,6 +1,21 @@
 import UIKit
 
 class MoveToNextPage: UIButton {
-
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureButton()
+    }
+    
+    private func configureButton() {
+        setTitle("다음", for: .normal)
+        setTitleColor(.systemGreen, for: .normal)
+        backgroundColor = UIColor.white
+        addTarget(superview, action: #selector(SignUpMainViewController.nextMoveButtonTouched(button:)), for: .touchUpInside)
+    }
 }

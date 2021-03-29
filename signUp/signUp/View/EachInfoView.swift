66efd27a34e3466fed4dac834e45905d61621ffc
Ray suh <readingod@naver.com>
2021-03-29 16:24:18 +0900

    StackView 추가

---
+++ signUp/signUp/View/EachInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  EachInfoView.swift
+//  signUp
+//
+//  Created by 서우석 on 2021/03/29.
+//
+
+import UIKit
+
+class EachInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

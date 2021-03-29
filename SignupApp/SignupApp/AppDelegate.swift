8f8bbacee7330b0192ee8dfcbc62f51406dffd7b
okstring <milkoi7801@gmail.com>
2021-03-29 19:54:57 +0900

    Implement: Make CustomTextField, UILable, Draw storyboard
     - set padding to textField
     - adopt UITextFieldDelegate

---
+++ SignupApp/SignupApp/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  SignupApp
+//
+//  Created by Issac on 2021/03/29.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+

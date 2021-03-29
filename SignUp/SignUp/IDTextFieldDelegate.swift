f0b6fee2a67f08a17cfa0ec0fa0275291160393f
zeke <yangsin1031@naver.com>
2021-03-29 18:02:42 +0900

    NotificationCenter Observer등록
    NotificationCenter Oberver등록, Post 구현

--- SignUp/SignUp/IDTextFieldDelegate.swift
+++ SignUp/SignUp/IDTextFieldDelegate.swift
@@ -19,13 +19,14 @@ class IDTextFieldDelegate: NSObject, UITextFieldDelegate {
         let extract = text.getArrayAfterRegex(regex: "[0-9a-z_-]+")
         if extract[0] == textField.text && text.count > 4 && text.count < 21 {
             textField.layer.borderWidth = 1.0
-            textField.layer.borderColor = UIColor.green.cgColor
+            textField.layer.borderColor = #colorLiteral(red: 0.1540483236, green: 0.6966413856, blue: 0.1375852525, alpha: 1)
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "Correct"), object: nil)
         } else {
             textField.layer.borderWidth = 1.0
-            textField.layer.borderColor = UIColor.red.cgColor
+            textField.layer.borderColor = #colorLiteral(red: 0.9967475533, green: 0.03828956559, blue: 0.05758263916, alpha: 1)
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "Incorrect"), object: nil)
         }
         return true
     }
 }
-
 
--- SignUp/SignUp/ViewController.swift
+++ SignUp/SignUp/ViewController.swift
@@ -9,13 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var IDTextField: UITextField!
+    @IBOutlet var IDCheckLabel: UILabel!
+    @IBOutlet var PWCheckLabel: UILabel!
+    @IBOutlet var PWReconfirmLabel: UILabel!
+    @IBOutlet var nameCheckLabel: UILabel!
+    
     var IdTextFieldDelegate = IDTextFieldDelegate()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         IDTextField.delegate = IdTextFieldDelegate
+        NotificationCenter.default.addObserver(self, selector: #selector(showCorrectID), name: NSNotification.Name.init(rawValue: "Correct"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(showIncorrectID), name: NSNotification.Name.init(rawValue: "Incorrect"), object: nil)
     }
 
-
+    @objc func showCorrectID() {
+        IDCheckLabel.text = "사용 가능합니다."
+        IDCheckLabel.textColor = #colorLiteral(red: 0.1540483236, green: 0.6966413856, blue: 0.1375852525, alpha: 1)
+    }
+    
+    @objc func showIncorrectID() {
+        IDCheckLabel.text = "5~20자의 영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다."
+        IDCheckLabel.textColor = #colorLiteral(red: 0.9967475533, green: 0.03828956559, blue: 0.05758263916, alpha: 1)
+    }
 }
 

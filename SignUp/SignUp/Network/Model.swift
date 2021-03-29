16a8b615365607ac354f38c042650540180aaf6e
hoonha choi <sosqsz2@naver.com>
2021-03-29 18:48:52 +0900

    비밀번호 매칭 추가

---
+++ SignUp/SignUp/Network/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  SignUp
+//
+//  Created by HOONHA CHOI on 2021/03/29.
+//
+
+import Foundation
+
+struct responseModel : Codable {
+    let result : String
+    let status : String
+}
--- SignUp/SignUp/ViewModel.swift
+++ SignUp/SignUp/ViewModel.swift
@@ -11,18 +11,35 @@ import Combine
 class ViewModel {
     
     @Published var idText = ""
-    @Published var pwText = ""
+    @Published var passwordText = ""
+    @Published var passwordConfirmText = ""
+    @Published var nameText = ""
     
     @Published var isIdValid = false
-    @Published var isPwValid = false
+    @Published var isPasswordValid = false
+    @Published var isPasswordConfirmValid = false
+    @Published var isNameValid = false
     
     private var cancellable = Set<AnyCancellable>()
+    
+    var isMatchPassword : AnyPublisher<Bool, Never> {
+        return self.$passwordText.combineLatest(self.$passwordConfirmText) {
+            return $0 == $1
+        }.eraseToAnyPublisher()
+    }
     
     init() {
         $idText.sink { (idText) in
             self.isIdValid = self.countInputString(idText)
         }.store(in: &cancellable)
         
+//        $passwordText.sink { (pwText) in
+//
+//        }.store(in: &cancellable)
+//
+            
+        $nameText.sink { (nameText) in
+        }.store(in: &cancellable)
     }
     
     func countInputString(_ string : String) -> Bool {

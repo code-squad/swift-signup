352f90223465ea24f3fb71986d1671dff4ce7a52
hoonha choi <sosqsz2@naver.com>
2021-03-29 14:14:36 +0900

    project create

---
+++ SignUp/SignUp/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  SignUp
+//
+//  Created by HOONHA CHOI on 2021/03/29.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+

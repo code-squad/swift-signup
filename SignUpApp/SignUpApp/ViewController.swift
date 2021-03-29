8e5f3c58892485e8541f12a10b5702f2d62125a9
Sonjh1306 <yale1306@gmail.com>
2021-03-29 14:50:50 +0900

    프로젝트 생성 및 불필요한 주석 제거

---
+++ SignUpApp/SignUpApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignUpApp
+//
+//  Created by sonjuhyeong on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+

fa4e82d5d80c1834b9ca3100692d6aeda7353ee6
dahun-lee-daji <ssss6596@gmail.com>
2021-03-29 16:14:14 +0900

    UI 뼈대 구성
    textfield의 height가 guideline보다 작은 문제 있음.

--- SignUp/SignUp/SignUpView/SignUpViewController.swift
+++ SignUp/SignUp/SignUpView/SignUpViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet var categoryLabelCollection: [UILabel]!
+    @IBOutlet var textFieldCollection: [UITextField]!
+    @IBOutlet var informationLabelCollection: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        configureCategorylabel()
+        configureTextField()
+        configureInformationLabel()
     }
 
+    func configureCategorylabel() {
+        for label in categoryLabelCollection {
+            label.font = .boldSystemFont(ofSize: 17)
+        }
+    }
+    
+    func configureTextField() {
+        for textfield in textFieldCollection {
+            textfield.borderStyle = .line
+        }
+    }
+    
+    func configureInformationLabel() {
+        for label in informationLabelCollection {
+            label.font = .systemFont(ofSize: 12)
+            label.isHidden = true
+        }
+    }
 
 }
 

95705a9be6054230b0395170eeef639fa766e470
dahun-lee-daji <ssss6596@gmail.com>
2021-03-29 18:05:07 +0900

    텍스트필드의 옵저버 패턴 구현
    상세 구현 예정

---
+++ SignUp/SignUp/SignUpView/TextFieldDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  TextFieldDelegate.swift
+//  SignUp
+//
+//  Created by 이다훈 on 2021/03/29.
+//
+
+import UIKit
+
+class TextFieldDelegate: NSObject, UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField.accessibilityIdentifier ?? "" {
+        case "PWTextField":
+            NotificationCenter.default.post(name: .pwTextField, object: textField)
+        case "PW2TextField":
+            NotificationCenter.default.post(name: .pw2TextField, object: textField)
+        case "IDTextField":
+            NotificationCenter.default.post(name: .idTextField, object: textField)
+        case "NameTextField":
+            NotificationCenter.default.post(name: .nameTextField, object: textField)
+        default:
+            return
+        }
+    }
+}

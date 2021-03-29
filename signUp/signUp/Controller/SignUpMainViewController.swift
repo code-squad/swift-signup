bf6e74158e0d67cebc9489f85549fd012b2adcdb
Ray suh <readingod@naver.com>
2021-03-29 16:24:18 +0900

    StackView 추가

--- signUp/signUp/Controller/SignUpMainViewController.swift
+++ signUp/signUp/Controller/SignUpMainViewController.swift
@@ -3,17 +3,20 @@ import UIKit
 class SignUpMainViewController: UIViewController {
     
     private var mainViewTitle: MainTitleLabel!
+    private var mainStackView = MainInfoStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setUpMainView()
-        
-        configureTitle()
     }
     
     private func setUpMainView() {
+        //MainView Background Color
         view.backgroundColor = UIColor.systemGray6
+        
+        configureTitle()
+        
+        configureMainStackView()
     }
 }
 
@@ -25,5 +28,14 @@ extension SignUpMainViewController {
     private func configureTitle() {
         mainViewTitle = MainTitleLabel(frame: CGRect(x: 125, y: 60, width: 150, height: 40), title: "회원가입")
         view.addSubview(mainViewTitle)
+    }
+    
+    private func configureMainStackView() {
+        view.addSubview(mainStackView)
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        mainStackView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+        mainStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 120).isActive = true
+        mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
     }
 }
--- signUp/signUp/View/MainInfoStackView.swift
+++ signUp/signUp/View/MainInfoStackView.swift
@@ -2,6 +2,12 @@ import UIKit
 
 class MainInfoStackView: UIStackView {
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
     
-
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }

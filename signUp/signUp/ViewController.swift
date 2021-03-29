1ad86f209fd1151da7d96ed91d5c9a608d5e8289
Ray suh <readingod@naver.com>
2021-03-29 14:33:46 +0900

    필요없는 주석 및 메서드 삭제

--- signUp/signUp/ViewController.swift
+++ signUp/signUp/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  signUp
-//
-//  Created by 서우석 on 2021/03/29.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 

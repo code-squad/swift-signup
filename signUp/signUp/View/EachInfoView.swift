4eea17cfa272a84af19d501a02f300241c6674b8
Ray suh <readingod@naver.com>
2021-03-29 17:19:58 +0900

    회원가입 스텍뷰 코드작성

--- signUp/signUp/View/EachInfoView.swift
+++ signUp/signUp/View/EachInfoView.swift
@@ -1,20 +1,44 @@
-//
-//  EachInfoView.swift
-//  signUp
-//
-//  Created by 서우석 on 2021/03/29.
-//
-
 import UIKit
 
 class EachInfoView: UIView {
+    
+    var infoLabel = UILabel()
+    var inputTextField = UITextField()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubViews()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubViews()
+    }
+    
+    private func setUpSubViews() {
+        setUpInfoLabel()
+        setUpTextField()
+        configrueSelfView()
+    }
+    
+    private func configrueSelfView() {
+        let height = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 80)
+        addConstraint(height)
+    }
+    
+    private func setUpInfoLabel() {
+        infoLabel.textAlignment = .left
+        infoLabel.textColor = UIColor.black
+        infoLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        infoLabel.frame = CGRect(x: 0, y: 0, width: 140, height: 40)
+        addSubview(infoLabel)
+    }
+    
+    private func setUpTextField() {
+        inputTextField.textAlignment = .left
+        inputTextField.textColor = UIColor.systemGray2
+        inputTextField.backgroundColor = UIColor.white
+        inputTextField.frame = CGRect(x: 0, y: 40, width: 300, height: 40)
+        addSubview(inputTextField)
+    }
 }

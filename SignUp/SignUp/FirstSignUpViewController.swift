2a4b1e4aef651db804d9cff072cc854e2a15ca73
HongzCloud <jihong21511@gmail.com>
2021-03-30 00:35:54 +0900

    💄 Add TextFieldDelegate
    텍스트필드를 동적으로 바꾸기 위해 델리게이트를 만들어 줬다.

--- SignUp/SignUp/FirstSignUpViewController.swift
+++ SignUp/SignUp/FirstSignUpViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class FirstSignUpViewController: UIViewController {
+class FirstSignUpViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var textFields: [UITextField]!
+    let textFieldDelegate = TextFieldDelegate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTextFields()
     }
 
-
+    private func configureTextFields() {
+        textFields.forEach { textField in
+            textField.layer.borderColor = CGColor(red: 112, green: 112, blue: 112, alpha: 1)
+            textField.layer.borderWidth = 1.0
+            textField.delegate = textFieldDelegate
+        }
+    }
 }
-

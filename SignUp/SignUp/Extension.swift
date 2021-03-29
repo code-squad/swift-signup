f387a0cddeab571280bcce0d0aa82d149f4b77b7
zeke <yangsin1031@naver.com>
2021-03-29 16:59:58 +0900

    Extension String
    정규표현식을 패턴에 따라 사용하기위해 String타입에 extension 하여 method로 작성

---
+++ SignUp/SignUp/Extension.swift
@@ -0,0 +1,25 @@
+//
+//  extension.swift
+//  SignUp
+//
+//  Created by 양준혁 on 2021/03/29.
+//
+
+import Foundation
+
+extension String{
+    func getArrayAfterRegex(regex: String) -> [String] {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: self,
+                                        range: NSRange(self.startIndex..., in: self))
+            return results.map {
+                String(self[Range($0.range, in: self)!])
+            }
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+}

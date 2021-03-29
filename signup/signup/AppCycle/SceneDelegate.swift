0d7bb1d7abbf7c97f85bcc2e6d57b01f5e1123b7
Song Lee <songlish233@gmail.com>
2021-03-29 14:45:26 +0900

    Configure Xcode project files

---
+++ signup/signup/AppCycle/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  signup
+//
+//  Created by Song on 2021/03/29.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
+

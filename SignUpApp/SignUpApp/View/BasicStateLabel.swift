52781b0a71c36fda86eb7be1a21ab90a3f773075
Sonjh1306 <yale1306@gmail.com>
2021-03-30 23:15:09 +0900

    BasicTextField, BasicStateLabel, BasicButton 추가,PwCheckTextFieldDelegate 수정

---
+++ SignUpApp/SignUpApp/View/BasicStateLabel.swift
@@ -0,0 +1,24 @@
+//
+//  BasicStateLabel.swift
+//  SignUpApp
+//
+//  Created by sonjuhyeong on 2021/03/30.
+//
+
+import UIKit
+
+class BasicStateLabel: UILabel {
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    func configure() {
+        self.textColor = .systemBackground
+    }
+}

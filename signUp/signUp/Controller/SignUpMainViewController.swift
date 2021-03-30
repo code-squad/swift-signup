fcb539eb26d066661646589d18f1af23cdc212d4
Ray suh <readingod@naver.com>
2021-03-30 14:27:17 +0900

    Feedback 수정 super.touchesBegan()

--- signUp/signUp/Controller/SignUpMainViewController.swift
+++ signUp/signUp/Controller/SignUpMainViewController.swift
@@ -13,6 +13,7 @@ class SignUpMainViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         self.view.endEditing(true)
     }
     

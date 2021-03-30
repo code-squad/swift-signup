5973456a65308bed14968561a66d51cfc5cab317
Song Lee <songlish233@gmail.com>
2021-03-30 15:11:20 +0900

    Add empty action for nextButton to configure template

--- signup/signup/Controller/SignUpViewController.swift
+++ signup/signup/Controller/SignUpViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class SignupViewController: UIViewController {
-
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet var signUpTextFieldModel: SignUpTextFieldModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        if signUpTextFieldModel.isAllFieldValidate() {
+            //다음으로 넘어갈 준비가 되었다!
+        }
+    }
 }
-

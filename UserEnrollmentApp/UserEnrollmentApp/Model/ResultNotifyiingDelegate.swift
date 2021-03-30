f526a4518f62befc0410f2f2e2309def18e1df72
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-30 10:37:44 +0900

    Implement delegation passing over validation result
    Created ResultNotifyingDelegate as protocol
    Made SignUpSceneIdFieldDelegate has the delegate

---
+++ UserEnrollmentApp/UserEnrollmentApp/Model/ResultNotifyiingDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  TextFieldsWithValidation.swift
+//  UserEnrollmentApp
+//
+//  Created by 조중윤 on 2021/03/29.
+//
+
+import UIKit
+
+protocol ResultNotifyingDelegate {
+    func showValidationResult(sender: UITextFieldDelegate, result: String)
+}

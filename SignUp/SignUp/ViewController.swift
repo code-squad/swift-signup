c83c26ad6000f7a7ac245d43c16bcbb9434f6a8e
lena <sue822330@gmail.com>
2021-03-30 14:55:01 +0900

    add signup view on storyboard

--- SignUp/SignUp/ViewController.swift
+++ SignUp/SignUp/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputForId: UITextField!
+    @IBOutlet weak var inputForPassword: UITextField!
+    @IBOutlet weak var messageForId: UILabel!
+    @IBOutlet weak var messageForPassword: UILabel!
+
+    private let idFielddelegate = IdFieldDelegate()
+    private let passwordFieldDelegate = PasswordFieldDelegate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }

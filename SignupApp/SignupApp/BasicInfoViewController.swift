7360f8267c465893170cee521aee32669080f4a8
1song2 <1song2918@gmail.com>
2021-03-30 16:57:28 +0900

    Check if user ID already exists
    Use GET method to retrieve user data.
    Update text and color of message label.

--- SignupApp/SignupApp/BasicInfoViewController.swift
+++ SignupApp/SignupApp/BasicInfoViewController.swift
@@ -19,12 +19,63 @@ class BasicInfoViewController: UIViewController {
     @IBOutlet weak var nameMessageLabel: UILabel!
     
     @IBOutlet weak var nextButton: UIButton!
-
+    
+    private var userIDs: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        idTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordConfirmTextField.delegate = self
+        nameTextField.delegate = self
+        
+        performRequest(with: "https://8r6ruzgzve.execute-api.ap-northeast-2.amazonaws.com/default/SwiftCamp")
+        
+        idMessageLabel.text = ""
+        passwordTextField.text = ""
+        passwordConfirmTextField.text = ""
+        nameTextField.text = ""
     }
-
-
+    
+    func performRequest(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard error == nil else { return }
+            guard let data = data else { return }
+            guard let response = response as? HTTPURLResponse, (200..<299) ~= response.statusCode else { return }
+            guard let ids = self.parseJSON(data) else { return }
+            self.userIDs = ids
+        }
+        task.resume()
+    }
+    
+    func parseJSON(_ userData: Data) -> [String]? {
+        do {
+            guard let jsonObject = try JSONSerialization.jsonObject(with: userData) as? [String] else {
+                print("Error: Cannot convert data to JSON object")
+                return nil
+            }
+            return jsonObject
+        } catch {
+            print("Error: Trying to convert JSON data to string")
+            return nil
+        }
+    }
 }
 
+extension BasicInfoViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let id = idTextField.text else { return }
+        if userIDs.contains(id) {
+            idMessageLabel.text = "이미 사용중인 아이디입니다."
+            idMessageLabel.textColor = UIColor(named: "redErrorMessage")
+        } else {
+            idMessageLabel.text = "사용 가능한 아이디입니다."
+            idMessageLabel.textColor = UIColor(named: "greenText")
+        }
+    }
+}

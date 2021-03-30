e6fc72880f48bfe03be5d84d9fe7314600bf56fb
1song2 <1song2918@gmail.com>
2021-03-30 15:16:23 +0900

    Connect IBOutlets

--- SignupApp/SignupApp/BasicInfoViewController.swift
+++ SignupApp/SignupApp/BasicInfoViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class BasicInfoViewController: UIViewController {
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var idMessageLabel: UILabel!
+    @IBOutlet weak var passwordMessageLabel: UILabel!
+    @IBOutlet weak var passwordConfirmMessageLabel: UILabel!
+    @IBOutlet weak var nameMessageLabel: UILabel!
+    
+    @IBOutlet weak var nextButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()

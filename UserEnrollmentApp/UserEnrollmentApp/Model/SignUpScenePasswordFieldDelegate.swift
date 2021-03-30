51095a7adbf857ea2ad9c2116362f711b3fb2daa
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-30 23:48:11 +0900

    Implement password validation

--- UserEnrollmentApp/UserEnrollmentApp/Model/SignUpScenePasswordFieldDelegate.swift
+++ UserEnrollmentApp/UserEnrollmentApp/Model/SignUpScenePasswordFieldDelegate.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class SignUpScenePasswordFieldDelegate: NSObject, UITextFieldDelegate {
+    public var resultNotifyingDelegate: ResultNotifyingDelegate?
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
         print("textFieldDidBeginEditing: \((textField.text) ?? "Empty")")
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
-        print("textFieldDidEndEditing: \((textField.text) ?? "Empty")")
+        if let inputText = textField.text {
+            var explanation: String
+            var validationResult: Bool
+            
+            guard UserInputValidator.validatePasswordLength(inputText) else {
+                explanation = "8자 이상 16자 이하로 입력해주세요."
+                validationResult = false
+                resultNotifyingDelegate?.showValidationResult(sender: self, result: validationResult, explanation: "\(explanation)")
+                return
+            }
+            
+            guard UserInputValidator.validatePasswordContainsCapitalLetter(inputText) else {
+                print("sdfsdf")
+                explanation = "영문 대문자를 최소 1자 이상 포함해주세요."
+                validationResult = false
+                resultNotifyingDelegate?.showValidationResult(sender: self, result: validationResult, explanation: "\(explanation)")
+                return
+            }
+            
+            guard UserInputValidator.validatePasswordContainsNumber(inputText) else {
+                explanation = "숫자를 최소 1자 이상 포함해주세요."
+                validationResult = false
+                resultNotifyingDelegate?.showValidationResult(sender: self, result: validationResult, explanation: "\(explanation)")
+                return
+            }
+            
+            guard UserInputValidator.validatePasswordContainsSpecialCharacter(inputText) else {
+                explanation = "특수문자를 최소 1자 이상 포함해주세요."
+                validationResult = false
+                resultNotifyingDelegate?.showValidationResult(sender: self, result: validationResult, explanation: "\(explanation)")
+                return
+            }
+            
+            explanation = "안전한 비밀번호입니다."
+            validationResult = true
+            resultNotifyingDelegate?.showValidationResult(sender: self, result: validationResult, explanation: "\(explanation)")
+            return
+        }
     }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print("textFieldShouldReturn \((textField.text) ?? "Empty")")
         textField.resignFirstResponder()
--- UserEnrollmentApp/UserEnrollmentAppTests/UserInputValidatorTests.swift
+++ UserEnrollmentApp/UserEnrollmentAppTests/UserInputValidatorTests.swift
@@ -9,14 +9,11 @@ import XCTest
 @testable import UserEnrollmentApp
 
 class UserInputValidatorTests: XCTestCase {
-    var idRegex: String!
     
     override func setUp() {
-        self.idRegex = "[a-z0-9_)(-]{5,20}"
     }
 
     override func tearDown() {
-        self.idRegex = nil
     }
 
     func test_invalid_id_is_invalid() {
@@ -24,9 +21,9 @@ class UserInputValidatorTests: XCTestCase {
         let invalidId2 = "sdfwsdfwsdfwsdfwsdfwsdfwsdfwsdfw"
         let invalidId3 = "sdsdfdfsdf!!!@#@#@"
         
-        XCTAssertFalse(UserInputValidator.valiidateId(invalidId1, with: idRegex))
-        XCTAssertFalse(UserInputValidator.valiidateId(invalidId2, with: idRegex))
-        XCTAssertFalse(UserInputValidator.valiidateId(invalidId3, with: idRegex))
+        XCTAssertFalse(UserInputValidator.validateId(invalidId1))
+        XCTAssertFalse(UserInputValidator.validateId(invalidId2))
+        XCTAssertFalse(UserInputValidator.validateId(invalidId3))
     }
     
     func test_valid_id_is_valid() {
@@ -34,8 +31,42 @@ class UserInputValidatorTests: XCTestCase {
         let validId2 = "12345-_-"
         let validId3 = "sddjfksldfjsdie("
         
-        XCTAssertTrue(UserInputValidator.valiidateId(validId1, with: idRegex))
-        XCTAssertTrue(UserInputValidator.valiidateId(validId2, with: idRegex))
-        XCTAssertTrue(UserInputValidator.valiidateId(validId3, with: idRegex))
+        XCTAssertTrue(UserInputValidator.validateId(validId1))
+        XCTAssertTrue(UserInputValidator.validateId(validId2))
+        XCTAssertTrue(UserInputValidator.validateId(validId3))
+    }
+    
+    func test_password_is_short_or_long() {
+        let shortPassword = "1233jdf"
+        let longPassword = "1233jdf1233jdf1233jdf1233jdf"
+        
+        XCTAssertFalse(UserInputValidator.validatePasswordLength(shortPassword))
+        XCTAssertFalse(UserInputValidator.validatePasswordLength(longPassword))
+    }
+    
+    func test_password_does_not_contain_capital_letter() {
+        let passwordWithoutCapitalLetter1 = "sdfsdf123123"
+        let passwordWithoutCapitalLetter2 = "sdfsd%f123123"
+        
+        XCTAssertFalse(UserInputValidator.validatePasswordContainsCapitalLetter(passwordWithoutCapitalLetter1))
+        XCTAssertFalse(UserInputValidator.validatePasswordContainsCapitalLetter(passwordWithoutCapitalLetter2))
+    }
+    
+    func test_password_does_not_contain_number() {
+        let passwordWithoutNumber1 = "sdfsdfsdfsdf"
+        let passwordWithoutNumber2 = "sdfsd%fsdfdf"
+        let passwordWithNumber1 = "123123sdf"
+        
+        XCTAssertFalse(UserInputValidator.validatePasswordContainsNumber(passwordWithoutNumber1))
+        XCTAssertFalse(UserInputValidator.validatePasswordContainsNumber(passwordWithoutNumber2))
+        XCTAssertTrue(UserInputValidator.validatePasswordContainsNumber(passwordWithNumber1))
+    }
+    
+    func test_password_does_not_contain_special_letter() {
+        let passwordWithoutSpecialLetter1 = "sdfsdf123123"
+        let passwordWithSpecialLetter1 = "sdfsd%f123123"
+        
+        XCTAssertFalse(UserInputValidator.validatePasswordContainsSpecialCharacter(passwordWithoutSpecialLetter1))
+        XCTAssertTrue(UserInputValidator.validatePasswordContainsSpecialCharacter(passwordWithSpecialLetter1))
     }
 }

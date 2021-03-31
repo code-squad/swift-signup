5f8d4f2d2097579e15b0f1ecded7c9dbe69b305d
Ray suh <readingod@naver.com>
2021-03-31 17:38:07 +0900

    서버에서 ID가져와서 비교 후 중복여부 확인 구현 완료

--- signUp/signUp/InfoCheck.swift
+++ signUp/signUp/InfoCheck.swift
@@ -3,12 +3,14 @@ import Foundation
 enum IdCheck: CustomStringConvertible {
     case idCount
     case nonSupportedValue
+    case doubleCheck
     case valid
     
     var description: String {
         switch self {
         case .idCount: return "5~20자의 영문 소문자, 숫자, 특수문자(_,-)만 사용 가능합니다."
         case .nonSupportedValue: return "영문 소문자, 숫자, 특수문자(_,-)만 사용 가능합니다."
+        case .doubleCheck: return "중복된 아이디입니다."
         case .valid: return "사용 가능한 아이디입니다."
         }
     }

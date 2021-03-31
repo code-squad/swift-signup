5fedc019a84ada3e58ac11fe8f158f18048f7a65
BMO <b38238496@gmail.com>
2021-03-31 14:43:34 +0900

    ButtonManager 생성
    모든 텍스트필드가 활성화 됐을 때 버튼을 활성화 시켜주는 클래스

--- swift-signup/swift-signup/ViewController.swift
+++ swift-signup/swift-signup/ViewController.swift
@@ -19,18 +19,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordConfirmText: PasswordConfirmTextField!
     @IBOutlet weak var passwordConfirmLabel: UILabel!
     
+    @IBOutlet weak var nameText: NameTextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    private var buttonManager = ButtonManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.idText.delegate = idText
-        self.passwordText.delegate = passwordText
-        self.passwordConfirmText.delegate = passwordConfirmText
+
         
         NotificationCenter.default.addObserver(self, selector: #selector(idGuideText), name: CustomTextField.idState, object: idText)
         NotificationCenter.default.addObserver(self, selector: #selector(passwordGuideText), name: CustomTextField.passwordState, object: passwordText)
         NotificationCenter.default.addObserver(self, selector: #selector(passwordConfirmGuideText), name: CustomTextField.passwordConfirmState, object: passwordConfirmText)
+        NotificationCenter.default.addObserver(self, selector: #selector(nameGuideText), name: CustomTextField.nameState, object: nameText)
+        NotificationCenter.default.addObserver(self, selector: #selector(buttonState), name: ButtonManager.buttonState, object: buttonManager)
+        
+        registerDelegate()
+        registerTextField()
     }
 
+    func registerDelegate() {
+        self.idText.delegate = idText
+        self.passwordText.delegate = passwordText
+        self.passwordConfirmText.delegate = passwordConfirmText
+        self.nameText.delegate = nameText
+    }
+    
+    func registerTextField() {
+        self.buttonManager.update(textField: type(of: idText), state: idText.currentState)
+        self.buttonManager.update(textField: type(of: passwordText), state: passwordText.currentState)
+        self.buttonManager.update(textField: type(of: passwordConfirmText), state: passwordConfirmText.currentState)
+        self.buttonManager.update(textField: type(of: nameText), state: nameText.currentState)
+    }
+    
     @objc
     func idGuideText(notification: Notification) {
         guard let text = notification.userInfo?[CustomTextField.MessageInfo.text] as? String,
@@ -40,6 +63,7 @@ class ViewController: UIViewController {
         }
         self.idLabel.text = text
         self.idLabel.textColor = color
+        buttonManager.update(textField: type(of: idText), state: idText.currentState)
     }
     
     @objc
@@ -53,6 +77,7 @@ class ViewController: UIViewController {
         self.passwordLabel.text = text
         self.passwordLabel.textColor = color
         self.passwordConfirmText.update(password: password)
+        buttonManager.update(textField: type(of: passwordText), state: passwordText.currentState)
     }
     
     @objc
@@ -64,6 +89,29 @@ class ViewController: UIViewController {
         }
         self.passwordConfirmLabel.text = text
         self.passwordConfirmLabel.textColor = color
+        buttonManager.update(textField: type(of: passwordConfirmText), state: passwordConfirmText.currentState)
     }
+    
+    @objc
+    func nameGuideText(notification: Notification) {
+        guard let text = notification.userInfo?[CustomTextField.MessageInfo.text] as? String,
+              let color = notification.userInfo?[CustomTextField.MessageInfo.color] as? UIColor
+        else {
+            return
+        }
+        self.nameLabel.text = text
+        self.nameLabel.textColor = color
+        buttonManager.update(textField: type(of: nameText), state: nameText.currentState)
+    }
+    
+    @objc
+    func buttonState(notification: Notification) {
+        guard let isActive = notification.userInfo?[ButtonManager.Info.state] as? Bool
+        else {
+            return
+        }
+        self.nextButton.isEnabled = isActive
+    }
+    
 }
 

144918d7ccbf6348c75d5c20d6c0f7e46d4dbf1d
okstring <milkoi7801@gmail.com>
2021-04-01 01:15:21 +0900

    Simple: JSON 처리 구분

--- SignupApp/SignupApp/Network/NetworkCenter.swift
+++ SignupApp/SignupApp/Network/NetworkCenter.swift
@@ -10,24 +10,20 @@ import Foundation
 class NetworkCenter {
     static let shared: NetworkCenter = NetworkCenter()
     
-    public func receiveIDList(url: URL, callback: @escaping ([String]) -> ()) {
+    public func receiveIDData(url: URL, callback: @escaping (Data) -> ()) {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
-        var IDList = Array<String>()
+        
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
             guard error == nil else { print(error!.localizedDescription); return }
             guard let data = data else { return }
-            do {
-                IDList = try JSONDecoder().decode([String].self, from: data)
-            } catch {
-                print(error.localizedDescription)
-                return
-            }
             
             DispatchQueue.main.async {
-                callback(IDList)
+                callback(data)
             }
         }
+        
         task.resume()
     }
 }

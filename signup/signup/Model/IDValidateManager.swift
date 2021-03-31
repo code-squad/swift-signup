82f8e399d6a44a22347c11f2da56a38ec9776f96
Song Lee <songlish233@gmail.com>
2021-03-31 17:38:42 +0900

    Delete ErrorManger, Add ValidateManager
    - ValidateManager controls valid states not only invalid states

---
+++ signup/signup/Model/IDValidateManager.swift
@@ -0,0 +1,74 @@
+//
+//  IDValidateManager.swift
+//  signup
+//
+//  Created by Song on 2021/03/31.
+//
+
+import Foundation
+
+class IDValidateManager: ValidateManager {
+    
+    private var status: Status
+    private let invalidMessages: [String]
+    private let validMessage: String
+    private let networkManager: NetworkManager
+    
+    init(status: Status, invalidMessages: [String], validMessage: String, networkManager: NetworkManager) {
+        self.status = status
+        self.invalidMessages = invalidMessages
+        self.validMessage = validMessage
+        self.networkManager = networkManager
+    }
+    
+    convenience init() {
+        let idStyleInvalid = "5~20자의 소문자, 숫자, 특수문자(_,-)만 입력 가능"
+        let serverProblem = "일시적으로 서버 데이터를 확인할 수 없습니다"
+        let idAlreadyExisting = "이미 존재하는 아이디입니다"
+        
+        let idValid = "사용 가능한 아이디입니다"
+        
+        let baseAddress = "https://8r6ruzgzve.execute-api.ap-northeast-2.amazonaws.com/default/SwiftCamp"
+        
+        self.init(status: Status(isValidated: false, message: ""),
+                  invalidMessages: [idStyleInvalid,
+                                    serverProblem,
+                                    idAlreadyExisting],
+                  validMessage: idValid,
+                  networkManager: NetworkManager(baseAddress: baseAddress))
+    }
+    
+    func check(_ inputs: [String]) -> Status {
+        let id = inputs[0]
+        status.isValidated = false
+        
+        guard let isExisting = isExisting(id) else {
+            status.message = invalidMessages[1]
+            return status
+        }
+        
+        if !isStyleValid(id) {
+            status.message = invalidMessages[0]
+        } else if isExisting {
+            status.message = invalidMessages[2]
+        } else {
+            status.isValidated = true
+            status.message = validMessage
+        }
+        return status
+    }
+    
+    private func isStyleValid(_ id: String) -> Bool {
+        let regex = try! NSRegularExpression(pattern: "^[a-z0-9_-]{5,20}$",
+                                             options: .anchorsMatchLines)
+        let match = regex.firstMatch(in: id,
+                                     options: .reportCompletion,
+                                     range: .init(location: 0, length: id.count))
+        return match != nil ? true : false
+    }
+    
+    private func isExisting(_ id: String) -> Bool? {
+        let result = networkManager.isExisting(id: id, password: "")
+        return result
+    }
+}
---
+++ signup/signup/Model/PasswordValidateManager.swift
@@ -0,0 +1,87 @@
+//
+//  PasswordValidateManager.swift
+//  signup
+//
+//  Created by Song on 2021/03/31.
+//
+
+import Foundation
+
+class PasswordValidateManager: ValidateManager {
+    
+    private var status: Status
+    private let invalidMessages: [String]
+    private let validMessage: String
+
+    init(status: Status, invalidMessages: [String], validMessage: String) {
+        self.status = status
+        self.invalidMessages = invalidMessages
+        self.validMessage = validMessage
+    }
+    
+    convenience init() {
+        let countInvalid = "8자 이상 16자 이하로 입력해주세요"
+        let uppercaseNotIncluded = "영문 대문자를 1개 이상 포함해주세요"
+        let numberNotIncluded = "숫자를 1개 이상 포함해주세요"
+        let symbolNotIncluded = "특수문자를 1개 이상 포함해주세요"
+        
+        let passwordValid = "안전한 비밀번호입니다"
+        
+        self.init(status: Status(isValidated: false, message: ""),
+                  invalidMessages: [countInvalid,
+                                    uppercaseNotIncluded,
+                                    numberNotIncluded,
+                                    symbolNotIncluded],
+                  validMessage: passwordValid)
+    }
+    
+    func check(_ inputs: [String]) -> Status {
+        let password = inputs[0]
+        status.isValidated = false
+        
+        if !isCountValid(password) {
+            status.message = invalidMessages[0]
+        } else if !isUppercaseIncluded(password) {
+            status.message = invalidMessages[1]
+        } else if !isNumberIncluded(password) {
+            status.message = invalidMessages[2]
+        } else if !isSymbolIncluded(password) {
+            status.message = invalidMessages[3]
+        } else {
+            status.isValidated = true
+            status.message = validMessage
+        }
+        return status
+    }
+    
+    private func isCountValid(_ password: String) -> Bool {
+        return password.count >= 8 && password.count <= 16 ? true : false
+    }
+    
+    private func isUppercaseIncluded(_ password: String) -> Bool {
+        let regex = try! NSRegularExpression(pattern: "(?=.*[A-Z])",
+                                            options: .anchorsMatchLines)
+        let match = regex.firstMatch(in: password,
+                                     options: .reportCompletion,
+                                     range: .init(location: 0, length: password.count))
+        return match != nil ? true : false
+    }
+    
+    private func isNumberIncluded(_ password: String) -> Bool {
+        let regex = try! NSRegularExpression(pattern: "(?=.*[0-9])",
+                                            options: .anchorsMatchLines)
+        let match = regex.firstMatch(in: password,
+                                     options: .reportCompletion,
+                                     range: .init(location: 0, length: password.count))
+        return match != nil ? true : false
+    }
+    
+    private func isSymbolIncluded(_ password: String) -> Bool {
+        let regex = try! NSRegularExpression(pattern: "(?=.*[^a-zA-Z0-9가-힣])",
+                                            options: .anchorsMatchLines)
+        let match = regex.firstMatch(in: password,
+                                     options: .reportCompletion,
+                                     range: .init(location: 0, length: password.count))
+        return match != nil ? true : false
+    }
+}

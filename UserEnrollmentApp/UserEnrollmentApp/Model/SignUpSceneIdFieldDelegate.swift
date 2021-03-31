4513f86ebc83a9edf5c20eb41251174a19235c1b
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-31 19:27:39 +0900

    Implement id checking behavior
    Implemented a method looking up the id an user finished typing and excecute following behaviors making use of the result

--- UserEnrollmentApp/UserEnrollmentApp/Model/SignUpSceneIdFieldDelegate.swift
+++ UserEnrollmentApp/UserEnrollmentApp/Model/SignUpSceneIdFieldDelegate.swift
@@ -13,13 +13,36 @@ class SignUpSceneIdFieldDelegate: NSObject, UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
         print("textFieldDidBeginEditing: \((textField.text) ?? "Empty")")
     }
+    
     func textFieldDidEndEditing(_ textField: UITextField) {
         if let inputText = textField.text {
-            let validationResult = UserInputValidator.validateId(inputText)
-            let explanation = validationResult ? "사용 가능한 아이디입니다." : "5~20자의 영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다."
+            var validationResult: Bool = false
+            var explanation: String = ""
+            
+            UserInputValidator.lookupId(inputText) { (isDuplicated) in
+                if isDuplicated {
+                    explanation = "이미 존재하는 아이디입니다."
+                    validationResult = false
+                    DispatchQueue.main.async {
+                        self.resultNotifyingDelegate?.passValidationResult(sender: self, result: validationResult, explanation: "\(explanation)")
+                    }
+                    return
+                }
+            }
+            
+            guard UserInputValidator.validateId(inputText) else {
+                explanation = "8자 이상 16자 이하로 입력해주세요."
+                validationResult = false
+                resultNotifyingDelegate?.passValidationResult(sender: self, result: validationResult, explanation: "\(explanation)")
+                return
+            }
+            
+            validationResult = true
+            explanation = "사용 가능한 아이디입니다."
             resultNotifyingDelegate?.passValidationResult(sender: self, result: validationResult, explanation: "\(explanation)")
         }
     }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print("textFieldShouldReturn \((textField.text) ?? "Empty")")
         textField.resignFirstResponder()

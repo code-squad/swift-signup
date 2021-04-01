4c00f1886b1a25e7600e9899a834b43fd19edb83
Song Lee <songlish233@gmail.com>
2021-04-01 14:33:09 +0900

    Create ValidReturnable protocol
    - Delete SignUpInfo singleton

---
+++ signup/signup/Model/ValidReturnable.swift
@@ -0,0 +1,15 @@
+//
+//  ValidReturnable.swift
+//  signup
+//
+//  Created by Song on 2021/04/01.
+//
+
+import Foundation
+
+protocol ValidReturnable {
+    
+    func validatedInput() -> String?
+    
+}
+

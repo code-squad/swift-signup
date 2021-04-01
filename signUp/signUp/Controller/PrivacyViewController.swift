91d414aa7a0d28e84b647dbf3c0de751f55614da
Ray suh <readingod@naver.com>
2021-04-01 16:36:37 +0900

    PrivacyStackView 생성

--- signUp/signUp/Controller/PrivacyViewController.swift
+++ signUp/signUp/Controller/PrivacyViewController.swift
@@ -1,10 +1,32 @@
 import UIKit
 
 class PrivacyViewController: UIViewController {
-
+    
+    private var privacyViewTitle: MainTitleLabel!
+    private var privacyStackView = MainInfoStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.systemRed
+        
+        setUpMainView()
     }
     
+    private func setUpMainView() {
+        view.backgroundColor = UIColor.systemGray6
+        configureTitle()
+        configureMainStackView()
+    }
+}
+
+extension PrivacyViewController {
+    
+    private func configureTitle() {
+        privacyViewTitle = MainTitleLabel(frame: CGRect(x: 125, y: 60, width: 150, height: 40))
+        privacyViewTitle.text = "개인정보"
+        view.addSubview(privacyViewTitle)
+    }
+    private func configureMainStackView() {
+        privacyStackView.frame = CGRect(x: 40, y: 120, width: 300, height: 400)
+        view.addSubview(privacyStackView)
+    }
 }

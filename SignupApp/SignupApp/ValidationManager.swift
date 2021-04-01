8d99d126606fb550dd6cf19e15c252fdc16d9525
1song2 <1song2918@gmail.com>
2021-04-01 12:20:25 +0900

    Add struct 'ValidationManager'

--- SignupApp/SignupApp/ValidationManager.swift
+++ SignupApp/SignupApp/ValidationManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct ValidationManager {
+    func isAvailable(username: String) -> Bool {
+        return username.validate(with: K.Regex.username)
+    }
+    
+    func isSatisfiedLength(password: String) -> Bool {
+        return password.validate(with: K.Regex.passwordLength)
+    }
+    
+    func hasUppercase(password: String) -> Bool {
+        return password.validate(with: K.Regex.passwordUppercase)
+    }
+    
+    func hasDigit(password: String) -> Bool {
+        return password.validate(with: K.Regex.passwordDigit)
+    }
+    
+    func hasSpecialCharacter(password: String) -> Bool {
+        return password.validate(with: K.Regex.passwordSpecialCharacter)
+    }
+    
+    func isAvailable(password: String) -> Bool {
+        return password.validate(with: K.Regex.password)
+    }
+}

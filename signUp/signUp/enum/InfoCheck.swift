dfdf05e65dfc64f05a44b1f40bfd4d90b6f832ea
Ray suh <readingod@naver.com>
2021-04-01 11:42:52 +0900

    enum타입 description 삭제

--- signUp/signUp/enum/InfoCheck.swift
+++ signUp/signUp/enum/InfoCheck.swift
@@ -1,55 +1,24 @@
 import Foundation
 
-enum IdCheck: CustomStringConvertible {
-    case idCount
-    case nonSupportedValue
-    case doubleCheck
-    case valid
-    
-    var description: String {
-        switch self {
-        case .idCount: return "5~20자의 영문 소문자, 숫자, 특수문자(_,-)만 사용 가능합니다."
-        case .nonSupportedValue: return "영문 소문자, 숫자, 특수문자(_,-)만 사용 가능합니다."
-        case .doubleCheck: return "중복된 아이디입니다."
-        case .valid: return "사용 가능한 아이디입니다."
-        }
-    }
+enum IdCheck {
+    static let idCount = "5~20자의 영문 소문자, 숫자, 특수문자(_,-)만 사용 가능합니다."
+    static let nonSupportedValue = "영문 소문자, 숫자, 특수문자(_,-)만 사용 가능합니다."
+    static let doubleCheck = "중복된 아이디입니다."
+    static let valid = "사용 가능한 아이디입니다."
 }
 
-enum PasswordCheck: CustomStringConvertible {
-    case passwordCount
-    case notContainedSpecialCharacters
-    case valid
-    
-    var description: String {
-        switch self {
-        case .passwordCount: return "8~16자의 영문 대/소문자, 숫자, 특수문자(!@#$%)만 사용 가능합니다."
-        case .notContainedSpecialCharacters: return "반드시 특수문자(!@#$%)가 포함되어야 합니다."
-        case .valid: return "사용 가능한 비밀번호입니다."
-        }
-    }
+enum PasswordCheck {
+    static let passwordCount = "8~16자의 영문 대/소문자, 숫자, 특수문자(!@#$%)만 사용 가능합니다."
+    static let notContainedSpecialCharacters = "반드시 특수문자(!@#$%)가 포함되어야 합니다."
+    static let valid = "사용 가능한 비밀번호입니다."
 }
 
-enum PasswordConfirmCheck: CustomStringConvertible {
-    case notEqul
-    case valid
-    
-    var description: String {
-        switch self {
-        case .notEqul: return "비밀번호가 일치하지 않습니다."
-        case .valid: return "비밀번호가 일치합니다."
-        }
-    }
+enum PasswordConfirmCheck {
+    static let notEqul = "비밀번호가 일치하지 않습니다."
+    static let valid = "비밀번호가 일치합니다."
 }
 
-enum NameCheck: CustomStringConvertible {
-    case nameCount
-    case valid
-    
-    var description: String {
-        switch self {
-        case .nameCount : return "2자 이상의 한글 이름을 입력해주세요."
-        case .valid : return "사용 가능한 이름입니다."
-        }
-    }
+enum NameCheck{
+    static let nameCount = "2자 이상의 한글 이름을 입력해주세요."
+    static let valid = "사용 가능한 이름입니다."
 }

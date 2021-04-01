9a3c4d117c7c985793edceadb5580da0d49a6a70
Song Lee <songlish233@gmail.com>
2021-04-01 12:25:51 +0900

    Add ColorSet struct including static UIColors

---
+++ signup/signup/ColorSet.swift
@@ -0,0 +1,20 @@
+//
+//  ColorSet.swift
+//  signup
+//
+//  Created by Song on 2021/04/01.
+//
+
+import UIKit
+
+struct ColorSet {
+    
+    static let PointRed = UIColor(named: "PointRed")
+
+    static let PointBlue = UIColor(named: "PointBlue")
+    
+    static let PointGreen = UIColor(named: "PointGreen")
+    
+    static let PointGrey = UIColor(named: "PointGrey")
+    
+}

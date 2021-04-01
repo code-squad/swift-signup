510681b4432ec688013c86c7d31fc2b3e4932803
ellyheetov <yaaajcm@naver.com>
2021-04-01 23:30:02 +0900

    feat: add custom value in UIColor

---
+++ SignUp/Extension/UIColorExtension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColorExtension.swift
+//  SignUp
+//
+//  Created by 박혜원 on 2021/04/01.
+//
+
+import UIKit
+
+extension UIColor {
+    static var myGreen = UIColor(red: 0.00, green: 0.65, blue: 0.01, alpha: 1.00)
+}

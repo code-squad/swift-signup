05a588291a588f4e4de1e68b3d73b066754fa803
1song2 <1song2918@gmail.com>
2021-04-01 12:24:19 +0900

    Separate UITextFieldDelegates from ViewController

--- SignupApp/SignupApp/BasicInfoViewController.swift
+++ SignupApp/SignupApp/BasicInfoViewController.swift
@@ -8,39 +8,34 @@
 import UIKit
 
 class BasicInfoViewController: UIViewController {
-    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var passwordConfirmTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     
-    @IBOutlet weak var idMessageLabel: UILabel!
+    @IBOutlet weak var usernameMessageLabel: UILabel!
     @IBOutlet weak var passwordMessageLabel: UILabel!
-    @IBOutlet weak var passwordConfirmMessageLabel: UILabel!
+    @IBOutlet weak var confirmPasswordMessageLabel: UILabel!
     @IBOutlet weak var nameMessageLabel: UILabel!
     
     @IBOutlet weak var nextButton: UIButton!
     
-    private var textFields: [UITextField] {
-        return [idTextField, passwordTextField, passwordConfirmTextField, nameTextField]
-    }
-    private var userIDs: [String] = []
+    var usernameTextFieldDelegate: UsernameTextFieldDelegate?
+    var passwordTextFieldDelegate: PasswordTextFieldDelegate?
+    var confirmPasswordTextFieldDelegate: ConfirmPasswordTextFieldDelegate?
+    var nameTextFieldDelegate: NameTextFieldDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        idTextField.delegate = self
-        passwordTextField.delegate = self
-        passwordConfirmTextField.delegate = self
-        nameTextField.delegate = self
+        configureUsernameTextField()
+        configurePasswordTextField()
+        configureconfirmPasswordTextField()
+        configureNameTextField()
+        setDefaultTextOfMessageLabels()
         
-        passwordTextField.passwordRules = UITextInputPasswordRules(descriptor: "required: upper; allowed: lower; required: digit; required: [!@#$%]; minlength: 8; maxlength: 16;")
-        
-        performRequest(with: "https://8r6ruzgzve.execute-api.ap-northeast-2.amazonaws.com/default/SwiftCamp")
-        
-        idMessageLabel.text = ""
-        passwordTextField.text = ""
-        passwordConfirmTextField.text = ""
-        nameTextField.text = ""
+        NotificationCenter.default.addObserver(self, selector: #selector(updateButtonState), name: NotificationName.DidChangeValidation, object: nil)
+        nextButton.isEnabled = false
         
         navigationController?.navigationBar.isHidden = true
         
@@ -48,105 +43,83 @@ class BasicInfoViewController: UIViewController {
         view.addGestureRecognizer(tap)
     }
     
-    func performRequest(with urlString: String) {
-        guard let url = URL(string: urlString) else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            guard error == nil else { return }
-            guard let data = data else { return }
-            guard let response = response as? HTTPURLResponse, (200..<299) ~= response.statusCode else { return }
-            guard let ids = self.parseJSON(data) else { return }
-            self.userIDs = ids
-        }
-        task.resume()
+    func configureUsernameTextField() {
+        usernameTextFieldDelegate = UsernameTextFieldDelegate(
+            validUsername: {
+                self.usernameMessageLabel.text = K.SuccessMessage.username
+                self.usernameMessageLabel.textColor = UIColor(named: K.KeyColors.greenText)
+            },
+            invalidUsername: {
+                self.usernameMessageLabel.text = K.ErrorMessage.username
+                self.usernameMessageLabel.textColor = UIColor(named: K.KeyColors.redErrorMessage)
+            },
+            alreadyExistingUsername: {
+                self.usernameMessageLabel.text = K.ErrorMessage.alreadyExistingUsername
+                self.usernameMessageLabel.textColor = UIColor(named: K.KeyColors.redErrorMessage)
+            })
+        usernameTextField.delegate = self.usernameTextFieldDelegate
     }
     
-    func parseJSON(_ userData: Data) -> [String]? {
-        do {
-            guard let jsonObject = try JSONSerialization.jsonObject(with: userData) as? [String] else {
-                print("Error: Cannot convert data to JSON object")
-                return nil
-            }
-            return jsonObject
-        } catch {
-            print("Error: Trying to convert JSON data to string")
-            return nil
-        }
-    }
-}
-
-extension BasicInfoViewController: UITextFieldDelegate {
-    func textFieldDidChangeSelection(_ textField: UITextField) {
-        guard let id = idTextField.text else { return }
-        if userIDs.contains(id) {
-            idMessageLabel.text = "이미 사용중인 아이디입니다."
-            idMessageLabel.textColor = UIColor(named: "redErrorMessage")
-        } else {
-            let idRegex = "[a-z0-9_-]{5,20}"
-            let idValidation = id.validate(with: idRegex)
-            if idValidation {
-                idMessageLabel.text = "사용 가능한 아이디입니다."
-                idMessageLabel.textColor = UIColor(named: "greenText")
-            } else {
-                idMessageLabel.text = "5~20자의 영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다."
-                idMessageLabel.textColor = UIColor(named: "redErrorMessage")
-            }
-        }
-        
-        guard let password = passwordTextField.text else { return }
-        let passwordLengthRegex = ".{8,16}"
-        let passwordUppercaseRegex = "[^A-Z]*[A-Z].*"
-        let passwordDigitRegex = "[^0-9]*[0-9].*"
-        let passwordSpecialCharacterRegex = "[^!@#$%]*[!@#$%].*"
-        let passwordRegex = "[A-Za-z0-9!@#$%]*"
-        let passwordLengthValidation = password.validate(with: passwordLengthRegex)
-        let passwordUppercaseValidation = password.validate(with: passwordUppercaseRegex)
-        let passwordDigitValidation = password.validate(with: passwordDigitRegex)
-        let passwordSpecialCharacterValidation = password.validate(with: passwordSpecialCharacterRegex)
-        let passwordValidation = password.validate(with: passwordRegex)
-        if !passwordLengthValidation {
-            passwordMessageLabel.text = "8자 이상 16자 이하로 입력해주세요."
-            passwordMessageLabel.textColor = UIColor(named: "redErrorMessage")
-        } else if !passwordUppercaseValidation {
-            passwordMessageLabel.text = "영문 대문자를 최소 1자 이상 포함해주세요."
-            passwordMessageLabel.textColor = UIColor(named: "redErrorMessage")
-        } else if !passwordDigitValidation {
-            passwordMessageLabel.text = "숫자를 최소 1자 이상 포함해주세요."
-            passwordMessageLabel.textColor = UIColor(named: "redErrorMessage")
-        } else if !passwordSpecialCharacterValidation {
-            passwordMessageLabel.text = "특수문자를 최소 1자 이상 포함해주세요."
-            passwordMessageLabel.textColor = UIColor(named: "redErrorMessage")
-        } else if passwordValidation {
-            passwordMessageLabel.text = "안전한 비밀번호입니다."
-            passwordMessageLabel.textColor = UIColor(named: "greenText")
-        }
-        
-        guard let passwordConfirm = passwordConfirmTextField.text else { return }
-        if password == passwordConfirm {
-            passwordConfirmMessageLabel.text = "비밀번호가 일치합니다."
-            passwordConfirmMessageLabel.textColor = UIColor(named: "greenText")
-        } else {
-            passwordConfirmMessageLabel.text = "비밀번호가 일치하지 않습니다."
-            passwordConfirmMessageLabel.textColor = UIColor(named: "redErrorMessage")
-        }
-        
-        if !nameTextField.hasText {
-            nameMessageLabel.text = "이름은 필수 입력 항목입니다."
-            nameMessageLabel.textColor = UIColor(named: "redErrorMessage")
-        } else {
-            nameMessageLabel.text = ""
-        }
+    func configurePasswordTextField() {
+        passwordTextFieldDelegate = PasswordTextFieldDelegate(
+            invalidPasswordLength: {
+                self.passwordMessageLabel.text = K.ErrorMessage.passwordLength
+                self.passwordMessageLabel.textColor = UIColor(named: K.KeyColors.redErrorMessage)
+            },
+            noUppercasePassword: {
+                self.passwordMessageLabel.text = K.ErrorMessage.passwordUppercase
+                self.passwordMessageLabel.textColor = UIColor(named: K.KeyColors.redErrorMessage)
+            },
+            noDigitPassword: {
+                self.passwordMessageLabel.text = K.ErrorMessage.passwordDigit
+                self.passwordMessageLabel.textColor = UIColor(named: K.KeyColors.redErrorMessage)
+            },
+            noSpecialCharacterPassword: {
+                self.passwordMessageLabel.text = K.ErrorMessage.passwordSpecialCharacter
+                self.passwordMessageLabel.textColor = UIColor(named: K.KeyColors.redErrorMessage)
+            },
+            validPassword: {
+                self.passwordMessageLabel.text = K.SuccessMessage.password
+                self.passwordMessageLabel.textColor = UIColor(named: K.KeyColors.greenText)
+            })
+        passwordTextField.delegate = self.passwordTextFieldDelegate
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        guard let index = textFields.firstIndex(of: textField) else { return false }
-        if index == 3 {
-            performSegue(withIdentifier: "goToProfile", sender: self)
-        } else {
-            textFields[index + 1].becomeFirstResponder()
+    func configureconfirmPasswordTextField() {
+        confirmPasswordTextFieldDelegate = ConfirmPasswordTextFieldDelegate(
+            validConfirmPassword: {
+                self.confirmPasswordMessageLabel.text = K.SuccessMessage.confirmPassword
+                self.confirmPasswordMessageLabel.textColor = UIColor(named: K.KeyColors.greenText)
+            }, invalidConfirmPassword: {
+                self.confirmPasswordMessageLabel.text = K.ErrorMessage.confirmPassword
+                self.confirmPasswordMessageLabel.textColor = UIColor(named: K.KeyColors.redErrorMessage)
+            })
+        confirmPasswordTextField.delegate = self.confirmPasswordTextFieldDelegate
+    }
+    
+    func configureNameTextField() {
+        nameTextFieldDelegate = NameTextFieldDelegate(
+            validName: {
+                self.nameMessageLabel.text = K.SuccessMessage.name
+            }, invalidName: {
+                self.nameMessageLabel.text = K.ErrorMessage.name
+                self.nameMessageLabel.textColor = UIColor(named: K.KeyColors.redErrorMessage)
+            })
+        nameTextField.delegate = self.nameTextFieldDelegate
+    }
+    
+    func setDefaultTextOfMessageLabels() {
+        usernameMessageLabel.text = ""
+        passwordTextField.text = ""
+        confirmPasswordTextField.text = ""
+        nameTextField.text = ""
+    }
+    
+    @objc func updateButtonState() {
+        guard let isValidUsername = usernameTextFieldDelegate?.validated, let isValidPassword = passwordTextFieldDelegate?.validated, let isValiedConfirmPassword = confirmPasswordTextFieldDelegate?.validated, let isValidName = nameTextFieldDelegate?.validated else {
+            return
         }
-        return false
+        let isValidAllFields = isValidUsername && isValidPassword && isValiedConfirmPassword && isValidName
+        nextButton.isEnabled = isValidAllFields
     }
 }
--- SignupApp/SignupApp/NameTextFieldDelegate.swift
+++ SignupApp/SignupApp/NameTextFieldDelegate.swift
@@ -5,4 +5,31 @@
 //  Created by Song on 2021/03/31.
 //
 
-import Foundation
+import UIKit
+
+class NameTextFieldDelegate: NSObject, UITextFieldDelegate {
+    private let validName: () -> ()
+    private let invalidName: () -> ()
+    var validated: Bool = false
+    
+    init(validName: @escaping () -> (), invalidName: @escaping () -> ()) {
+        self.validName = validName
+        self.invalidName = invalidName
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let name = textField.text else { return }
+        validated = validate(text: name)
+        NotificationCenter.default.post(name: NotificationName.DidChangeValidation, object: validated)
+    }
+    
+    func validate(text: String) -> Bool {
+        if text.isEmpty {
+            invalidName()
+            return false
+        } else {
+            validName()
+            return true
+        }
+    }
+}

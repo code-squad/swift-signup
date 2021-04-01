b833d1b9e8a9d75229add3e1067de708c7491792
HongzCloud <jihong21511@gmail.com>
2021-04-01 17:23:45 +0900

    ✨ Add checking recheckingPwTextField,nameTextField function
    1. 비밀번호 재확인 텍스트 핃드 타이핑 시 작성한 비밀번호와 일치하는지 확인하는 로직 작성
    2. 다음 버튼을 누르면 모든 텍스트 필드를 확인하고 잘못된 텍스트필드의 라벨에 메시지를 띄움

--- SignUp/SignUp/ViewController/FirstSignUpViewController.swift
+++ SignUp/SignUp/ViewController/FirstSignUpViewController.swift
@@ -11,8 +11,13 @@ class FirstSignUpViewController: UIViewController {
     
     @IBOutlet weak var idTextField: SignUpTextField!
     @IBOutlet weak var pwTextFiled: SignUpTextField!
+    @IBOutlet weak var recheckingPwTextField: SignUpTextField!
+    @IBOutlet weak var nameTextField: SignUpTextField!
+    
     @IBOutlet weak var idResultLabel: UILabel!
     @IBOutlet weak var pwResultLabel: UILabel!
+    @IBOutlet weak var recheckPwResultLabel: UILabel!
+    @IBOutlet weak var nameResultLabel: UILabel!
     
     let idTextFieldDelegate = IdTextFieldDelegate()
     let pwTextFieldDelegate = PwTextFieldDelegate()
@@ -25,7 +30,7 @@ class FirstSignUpViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(updateIdResultLabel), name: IdTextFieldDelegate.notificationName, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(updatePwResultLabel), name: PwTextFieldDelegate.notificationName, object: nil)
     }
-
+    
     @objc
     private func updateIdResultLabel(notification: Notification) {
         guard let tempUserInfo = notification.userInfo,
@@ -46,8 +51,22 @@ class FirstSignUpViewController: UIViewController {
         updateLabel(pwResultLabel, condition: userInfo)
     }
     
+    @IBAction func editingChangedRecheckingPwTextField(_ sender: Any) {
+
+        let result = Validator.isSameText(first: pwTextFiled.text!, second: recheckingPwTextField.text!)
+        
+            updateLabel(recheckPwResultLabel, condition: result)
+    }
+    
+    @IBAction func didTabNextButton(_ sender: SignUpButton) {
+        canGoNextPage().forEach { label, result in
+            result.keys.forEach { isCorrect in
+                if isCorrect == false { updateLabel(label, condition: result) }
+            }
+        }
+    }
+    
     private func updateLabel(_ label: UILabel, condition: [Bool:String]) {
-        label.isHidden = false
         
         if let message = condition[true] {
             label.text = message
@@ -57,5 +76,13 @@ class FirstSignUpViewController: UIViewController {
             label.text = condition[false]
             label.textColor = .systemRed
         }
+    }
+    
+    private func canGoNextPage() -> [UILabel : [Bool:String]] {
+        let canGoNextPage = [idResultLabel! : Validator.isCorrected(id: idTextField.text!),
+                             pwResultLabel! : Validator.isCorrected(pw: pwTextFiled.text!),
+                             recheckPwResultLabel! : Validator.isSameText(first: pwResultLabel.text!, second: recheckPwResultLabel.text!),
+                             nameResultLabel! : Validator.isEmpty(nameTextField.text!)]
+        return canGoNextPage
     }
 }

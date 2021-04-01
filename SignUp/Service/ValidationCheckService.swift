858b0a343864513d0af2540dbbf700c8db8bd5b2
ellyheetov <yaaajcm@naver.com>
2021-04-01 23:00:03 +0900

    feat: add validation check funtion using regular expression

--- SignUp/Service/ValidationCheckService.swift
+++ SignUp/Service/ValidationCheckService.swift
@@ -7,29 +7,87 @@
 
 import Foundation
 
+
 struct ValidationCheckService {
+    typealias Length = (start : Int, end: Int)
     
-    static func isValidId(input text : String) -> Bool {
+    static let LengthOfPassword : Length = (5,20)
+    static let LengthOfId : Length = (8,16)
+        
+    static func isValidId(input text : String) -> IDValidState.State {
         // 영문 소문자, 숫자, 특수기호(_,-), 5-20자
-        let pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,16}"
-        let regex = try? NSRegularExpression(pattern: pattern)
-        if let _ = regex?.firstMatch(in: text, options: [], range: NSRange(location: 0, length: text.count)) {
-            return true
-        }else {
-            return false
+        if !isIncludeLower(text) {
+            return .inValid
+        } else {
+            return .valid
         }
     }
-    static func isValidPassword(input text : String) -> Bool {
+    static func isValidPassword(input text : String) -> PasswordValidState.State {
         // 영문 대/소문자, 숫자, 특수문자(!@#$%), 8-16자
-        let pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,16}"
-        let regex = try? NSRegularExpression(pattern: pattern)
-        if let _ = regex?.firstMatch(in: text, options: [], range: NSRange(location: 0, length: text.count)) {
+        if !isIncludeNumber(text) {
+            return .notIncludeNumber
+        } else if !isIncludeUpper(text) {
+            return .notIncludeUpperCase
+        } else if !isIncludeSpecial(text) {
+            return .notIncludeSpecialCharacter
+        } else if !isInRange(LengthOfPassword.start, LengthOfPassword.end, text) {
+            return .inValidRange
+        } else {
+            return .valid
+        }
+        
+    }
+    static func isValidName(input text: String) -> NameValidState.State {
+        if !text.isEmpty {
+            return .inValid
+        } else {
+            return .valid
+        }
+    }
+    
+    static let isIncludeNumber : (String) -> Bool = { text in
+        let pattern = "[:digit:]"
+        let regex2 = try? NSRegularExpression(pattern: pattern)
+        if let _ = regex2?.firstMatch(in: text, options: [], range: NSRange(location: 0, length: text.count)) {
             return true
-        }else {
+        } else {
             return false
         }
     }
-    static func isValidName(input text: String) -> Bool {
-        return !text.isEmpty
+    static let isIncludeLower : (String) -> Bool = { text in
+        let pattern = "[:lower:]"
+        let regex2 = try? NSRegularExpression(pattern: pattern)
+        if let _ = regex2?.firstMatch(in: text, options: [], range: NSRange(location: 0, length: text.count)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    static let isIncludeUpper : (String) -> Bool = { text in
+        let pattern = "[:upper:]"
+        let regex2 = try? NSRegularExpression(pattern: pattern)
+        if let _ = regex2?.firstMatch(in: text, options: [], range: NSRange(location: 0, length: text.count)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    static let isIncludeSpecial : (String) -> Bool = { text in
+        let pattern = "[:punct:]"
+        let regex2 = try? NSRegularExpression(pattern: pattern)
+        if let _ = regex2?.firstMatch(in: text, options: [], range: NSRange(location: 0, length: text.count)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    static let isInRange : (Int, Int, String) -> Bool = { start, end, text in
+        let pattern = "^().{\(start),\(end)}"
+        let regex2 = try? NSRegularExpression(pattern: pattern)
+        if let _ = regex2?.firstMatch(in: text, options: [], range: NSRange(location: 0, length: text.count)) {
+            return true
+        } else {
+            return false
+        }
     }
 }

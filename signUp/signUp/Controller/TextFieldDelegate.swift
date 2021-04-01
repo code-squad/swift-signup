6a30195b99d45665f1c3220412205ad020411d80
Ray suh <readingod@naver.com>
2021-04-01 15:03:35 +0900

    뷰 입력 -> 뷰컨트롤러에서 -> 모델의 매니저로 확인요청 -> 뷰컨트롤러에서 뷰 업데이트 호출로 코드 수정

--- signUp/signUp/Controller/TextFieldDelegate.swift
+++ signUp/signUp/Controller/TextFieldDelegate.swift
@@ -3,7 +3,6 @@ import UIKit
 class TextFieldDelegate: NSObject, UITextFieldDelegate {
 
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        textField.layer.borderWidth = 1.0
         return true
     }
 

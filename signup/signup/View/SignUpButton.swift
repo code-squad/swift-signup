814383d49d7237410da971deaa0aac583d53aeb1
Song Lee <songlish233@gmail.com>
2021-04-01 12:12:14 +0900

    Add methods about changing view states

--- signup/signup/View/SignUpButton.swift
+++ signup/signup/View/SignUpButton.swift
@@ -21,4 +21,19 @@ class SignUpButton: UIButton {
             layer.borderWidth = borderWidth
         }
     }
+    
+    func configureColors() {
+        setTitleColor(UIColor(named: "PointGreen"), for: .normal)
+        setTitleColor(UIColor(named: "PointGrey"), for: .disabled)
+    }
+    
+    func disable() {
+        isEnabled = false
+        tintColor = UIColor(named: "PointGrey")
+    }
+    
+    func enable() {
+        isEnabled = true
+        tintColor = UIColor(named: "PointGreen")
+    }
 }

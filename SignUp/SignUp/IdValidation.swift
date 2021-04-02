793dd161ddcd26c9d4c451f35fb3d1f4bb67a78b
hoonha choi <sosqsz2@naver.com>
2021-04-02 12:32:09 +0900

    불필요한 파일 삭제

--- SignUp/SignUp/IdValidation.swift
+++
@@ -1,8 +0,0 @@
-//
-//  IdValidation.swift
-//  SignUp
-//
-//  Created by HOONHA CHOI on 2021/03/31.
-//
-
-import Foundation
--- SignUp/SignUp/NameViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NameViewModel.swift
-//  SignUp
-//
-//  Created by HOONHA CHOI on 2021/03/31.
-//
-
-import Foundation

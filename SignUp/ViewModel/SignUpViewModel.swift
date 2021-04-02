d913d8ef3fbffce48ffedf89d4ad940492340ea1
ellyheetov <yaaajcm@naver.com>
2021-04-02 11:38:59 +0900

    refactor: update repassword textField data type

--- SignUp/ViewModel/SignUpViewModel.swift
+++ SignUp/ViewModel/SignUpViewModel.swift
@@ -11,17 +11,11 @@ import Combine
 class SignUpViewModel: NSObject {
     @IBOutlet weak var id: IDTextField!
     @IBOutlet weak var password: PasswordTextField!
-    @IBOutlet weak var repassword: UITextField!
+    @IBOutlet weak var repassword: RePasswordTextField!
     @IBOutlet weak var name: NameTextField!
     
     override init(){
         super.init()
     
-    }
-    func setDelegate(_ view : UITextFieldDelegate){
-        self.id.delegate = view
-        self.password.delegate = view
-        self.repassword.delegate = view
-        self.name.delegate = view
     }
 }

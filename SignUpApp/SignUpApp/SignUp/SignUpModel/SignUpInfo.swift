fa90cbcd53e553135998661c3b7c214adf9f1a88
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-04-05 17:19:05 +0900

    View에서 담는 기본적인 내용의 틀입니다.

--- SignUpApp/SignUpApp/SignUp/SignUpModel/SignUpInfo.swift
+++ SignUpApp/SignUpApp/SignUp/SignUpModel/SignUpInfo.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct SignUpInfo {
+    
+    let title: String
+    let placeholder: String
+    let warning: String
+    
+    init(title: String, placeholder: String, warning: String) {
+        self.title = title
+        self.placeholder = placeholder
+        self.warning = warning
+    }
+    
+}

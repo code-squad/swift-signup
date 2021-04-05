ea034d9f6c618134e052987c056846a520f2faa3
BMO <b38238496@gmail.com>
2021-04-05 19:48:39 +0900

    CustomTextFieldDelegate 클래스 생성
    클로저를 넘겨줄 타입

---
+++ swift-signup/swift-signup/TextFieldDelegate/CustomTextFieldDelegate.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+import UIKit
+
+class CustomTextFieldDelegate : NSObject, UITextFieldDelegate {
+    
+    private var text : ((String, NSRange, String) -> Void)!
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        self.text(textField.text ?? "", range, string)
+        return true
+    }
+    
+    func configureText(closure: @escaping ((String, NSRange, String) -> Void)) {
+        self.text = closure
+    }
+}

282276bbb1282f438a07f28e8ab875c15e57da0c
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-04-05 17:19:32 +0900

    View의 구성요소는 패턴이 있어 StackView로 추상화했습니다.

--- SignUpApp/SignUpApp/SignUp/SignUpView/StackViewUnit.swift
+++ SignUpApp/SignUpApp/SignUp/SignUpView/StackViewUnit.swift
@@ -9,12 +9,52 @@ import UIKit
 
 class StackViewUnit: UIStackView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var title: UILabel!
+    var textField: UITextField!
+    var subLabel: UILabel!
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        configureTitleLabel()
+        configureTextField()
+        configureSubLabel()
     }
-    */
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureTitleLabel()
+        configureTextField()
+        configureSubLabel()
+    }
+    
+    private func configureTitleLabel() {
+        title = UILabel()
+        title.textAlignment = .left
+        title.textColor = .black
+        title.font = UIFont(name: "HelveticaNeue-Bold", size: 14)
+        self.addArrangedSubview(title)
+    }
+    
+    private func configureTextField() {
+        textField = UITextField()
+        textField.layer.cornerRadius = 0
+        textField.addConstraint(textField.heightAnchor.constraint(equalToConstant: 38))
+        textField.addConstraint(textField.widthAnchor.constraint(equalToConstant: 302))
+        textField.borderStyle = .bezel
+        textField.keyboardType = .default
+        textField.returnKeyType = .next
+        
+        textField.attributedPlaceholder = NSAttributedString(string: "", attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 141, green: 141, blue: 141, alpha: 1)])
+        self.addArrangedSubview(textField)
+    }
+    
+    private func configureSubLabel() {
+        subLabel = UILabel()
+        subLabel.textAlignment = .left
+        subLabel.textColor = .black
+        subLabel.font = UIFont(name: "HelveticaNeue", size: 12)
+        self.addArrangedSubview(subLabel)
+    }
+    
 }
+

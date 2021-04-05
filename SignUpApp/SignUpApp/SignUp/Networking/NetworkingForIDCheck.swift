597df5538a3e50e8aca7b96c5dd655c9608c9690
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-04-05 17:18:01 +0900

    ID 중복확인을 위한 URLSession입니다.

--- SignUpApp/SignUpApp/SignUp/Networking/NetworkingForIDCheck.swift
+++ SignUpApp/SignUpApp/SignUp/Networking/NetworkingForIDCheck.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class NetworkingForIDCheck {
+    
+    static var idData = [String]()
+    
+    static func getData(completion: @escaping ([String]) -> ()) {
+        let URLString = "https://8r6ruzgzve.execute-api.ap-northeast-2.amazonaws.com/default/SwiftCamp"
+        if let url = URL(string: URLString) {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    do {
+                        let res = try JSONDecoder().decode([String].self, from: data)
+                        idData = res
+                        completion(res)
+                    } catch let error {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
+    }
+    
+}
